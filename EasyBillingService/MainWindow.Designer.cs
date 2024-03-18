a32afbf8e17be528dbd600c64491c65b419ff960
Linus Tiemann <43908801+Z3rca@users.noreply.github.com>
2024-03-18 23:44:23 +0100

    WIP first progress

--- EasyBillingService/MainWindow.Designer.cs
+++ EasyBillingService/MainWindow.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace EasyBillingService
 {
-    partial class Form1
+    partial class MainWindow
     {
         /// <summary>
         /// Required designer variable.
@@ -29,30 +29,44 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(268, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(119, 37);
+            this.button1.Location = new System.Drawing.Point(88, 32);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
+            this.button1.TabIndex = 2;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // Form1
+            // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(800, 286);
             this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Controls.Add(this.label1);
+            this.Location = new System.Drawing.Point(15, 15);
+            this.Name = "MainWindow";
+            this.Load += new System.EventHandler(this.MainWindow_Load);
             this.ResumeLayout(false);
         }
 
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
 
         #endregion
--- EasyBillingService/MainWindow.cs
+++ EasyBillingService/MainWindow.cs
@@ -1,30 +1,117 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+using MiniExcelLibs;
+using MiniExcelLibs.Attributes;
+using MiniExcelLibs.OpenXml;
+using Application = Microsoft.Office.Interop.Excel.Application;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace EasyBillingService
 {
-    public partial class Form1 : Form
+    public partial class MainWindow : Form
     {
-        public Form1()
+        public MainWindow()
         {
             InitializeComponent();
         }
+        
+        
+        static void Test()
+        {
+           
+            var excelApplication = new Microsoft.Office.Interop.Excel.Application();
+            excelApplication.Visible = true;
+
+			// Arbeitsmappe öffnen
+			object missing = Missing.Value;
+            var path = "H:\\C# Projects\\EasyBillingService\\EasyBillingService\\bin\\Debug\\";
+            
+			Workbook workbook = excelApplication.Workbooks.Open(path+"test.xlsx");
+
+
+
+            var sheet = workbook.ActiveSheet as Worksheet;
+
+            var cell = (double)sheet.Range["B3", "B3"].Value;
+
+            cell++;
+            
+            
+            
+            
+           
+            
+            
+            workbook.Save();
+			// Excel beenden
+
+            
+            workbook.Close(SaveChanges: true);
+            workbook = null;
+            excelApplication.Quit();
+            excelApplication = null;
+            
+			Console.WriteLine("Beenden mit Return");
+			Console.ReadLine();
+		}
 
         private void button1_Click(object sender, EventArgs e)
         {
-            throw new System.NotImplementedException();
+            
+            Test();
+            /*label1.Text = " clicked the button";
+
+            var table = new List<Entry>();
+            
+            var config = new OpenXmlConfiguration()
+            {
+                TableStyles = MiniExcelLibs.OpenXml.TableStyles.None,
+            };
+            
+            using (var stream = File.OpenRead("test.xlsx"))
+            { 
+                table = MiniExcel.Query<Entry>("test.xlsx").ToList();
+                
+           
+
+
+                table = table.Where(entry => entry.Name != null).ToList();
+
+                table[1].Value = 1;
+            }
+
+            MiniExcel.SaveAs("test.xlsx", table,true,"Sheet1",ExcelType.XLSX,null,true);*/
+        
+
+           // File.Replace("test_temp.xlsx", "test.xlsx", "test.xlsx", false);
+            
+           // File.Delete("test_temp.xlsx");
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+
+        public class Entry
+        {
+            public String Name { get; set; }
+            public int Value { get; set; }
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
         {
             throw new System.NotImplementedException();
         }
+        
+        
+        
+       
     }
 }

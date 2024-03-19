c894755e39195c97b7130fc856671005714edd5a
Linus Tiemann <linus-tiemann@live.de>
2024-03-19 20:48:55 +0100

    wip working read in

--- EasyBillingService/MainWindow.Designer.cs
+++ EasyBillingService/MainWindow.Designer.cs
@@ -29,44 +29,147 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.BillingBookLabel = new System.Windows.Forms.Label();
+            this.OpenBillingAdress = new System.Windows.Forms.Button();
+            this.SetValue = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.LastBillingNumber = new System.Windows.Forms.TextBox();
+            this.LastBillingAdressLabel = new System.Windows.Forms.Label();
+            this.DateLabel = new System.Windows.Forms.Label();
+            this.DateText = new System.Windows.Forms.TextBox();
+            this.RecipientLabel = new System.Windows.Forms.Label();
+            this.RecipientText = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // label1
+            // BillingBookLabel
             // 
-            this.label1.Location = new System.Drawing.Point(268, 37);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(100, 23);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.BillingBookLabel.Location = new System.Drawing.Point(203, 9);
+            this.BillingBookLabel.Name = "BillingBookLabel";
+            this.BillingBookLabel.Size = new System.Drawing.Size(280, 23);
+            this.BillingBookLabel.TabIndex = 1;
+            this.BillingBookLabel.Text = "Rechnungsadressbuch";
             // 
-            // button1
+            // OpenBillingAdress
             // 
-            this.button1.Location = new System.Drawing.Point(88, 32);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.OpenBillingAdress.Location = new System.Drawing.Point(88, 32);
+            this.OpenBillingAdress.Name = "OpenBillingAdress";
+            this.OpenBillingAdress.Size = new System.Drawing.Size(75, 23);
+            this.OpenBillingAdress.TabIndex = 2;
+            this.OpenBillingAdress.Text = "Öffnen...";
+            this.OpenBillingAdress.UseVisualStyleBackColor = true;
+            this.OpenBillingAdress.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // SetValue
+            // 
+            this.SetValue.Location = new System.Drawing.Point(98, 202);
+            this.SetValue.Name = "SetValue";
+            this.SetValue.Size = new System.Drawing.Size(75, 23);
+            this.SetValue.TabIndex = 2;
+            this.SetValue.Text = "Der Knopf";
+            this.SetValue.UseVisualStyleBackColor = true;
+            this.SetValue.Click += new System.EventHandler(this.SetValue_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(186, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(540, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // LastBillingNumber
+            // 
+            this.LastBillingNumber.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.LastBillingNumber.Enabled = false;
+            this.LastBillingNumber.Location = new System.Drawing.Point(88, 93);
+            this.LastBillingNumber.Name = "LastBillingNumber";
+            this.LastBillingNumber.Size = new System.Drawing.Size(100, 20);
+            this.LastBillingNumber.TabIndex = 4;
+            this.LastBillingNumber.TextChanged += new System.EventHandler(this.BillingNumber_TextChanged);
+            // 
+            // LastBillingAdressLabel
+            // 
+            this.LastBillingAdressLabel.Location = new System.Drawing.Point(88, 67);
+            this.LastBillingAdressLabel.Name = "LastBillingAdressLabel";
+            this.LastBillingAdressLabel.Size = new System.Drawing.Size(186, 23);
+            this.LastBillingAdressLabel.TabIndex = 1;
+            this.LastBillingAdressLabel.Text = "Letzte Adresse";
+            // 
+            // DateLabel
+            // 
+            this.DateLabel.Location = new System.Drawing.Point(203, 67);
+            this.DateLabel.Name = "DateLabel";
+            this.DateLabel.Size = new System.Drawing.Size(186, 23);
+            this.DateLabel.TabIndex = 1;
+            this.DateLabel.Text = "Datum";
+            // 
+            // DateText
+            // 
+            this.DateText.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.DateText.Enabled = false;
+            this.DateText.Location = new System.Drawing.Point(203, 93);
+            this.DateText.Name = "DateText";
+            this.DateText.Size = new System.Drawing.Size(100, 20);
+            this.DateText.TabIndex = 4;
+            this.DateText.TextChanged += new System.EventHandler(this.BillingNumber_TextChanged);
+            // 
+            // RecipientLabel
+            // 
+            this.RecipientLabel.Location = new System.Drawing.Point(309, 67);
+            this.RecipientLabel.Name = "RecipientLabel";
+            this.RecipientLabel.Size = new System.Drawing.Size(186, 23);
+            this.RecipientLabel.TabIndex = 1;
+            this.RecipientLabel.Text = "Rechnungs Empfänger";
+            // 
+            // RecipientText
+            // 
+            this.RecipientText.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.RecipientText.Enabled = false;
+            this.RecipientText.Location = new System.Drawing.Point(309, 93);
+            this.RecipientText.Name = "RecipientText";
+            this.RecipientText.Size = new System.Drawing.Size(100, 20);
+            this.RecipientText.TabIndex = 4;
+            this.RecipientText.TextChanged += new System.EventHandler(this.BillingNumber_TextChanged);
             // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
             this.ClientSize = new System.Drawing.Size(800, 286);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.RecipientText);
+            this.Controls.Add(this.DateText);
+            this.Controls.Add(this.LastBillingNumber);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.SetValue);
+            this.Controls.Add(this.OpenBillingAdress);
+            this.Controls.Add(this.RecipientLabel);
+            this.Controls.Add(this.DateLabel);
+            this.Controls.Add(this.LastBillingAdressLabel);
+            this.Controls.Add(this.BillingBookLabel);
             this.Location = new System.Drawing.Point(15, 15);
             this.Name = "MainWindow";
             this.Load += new System.EventHandler(this.MainWindow_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label RecipientLabel;
+        private System.Windows.Forms.TextBox RecipientText;
+
+        private System.Windows.Forms.Label DateLabel;
+        private System.Windows.Forms.TextBox DateText;
+
+        private System.Windows.Forms.Label BillingBookLabel;
+
+        private System.Windows.Forms.TextBox LastBillingNumber;
+
+        private System.Windows.Forms.TextBox textBox1;
+
+        private System.Windows.Forms.Button SetValue;
+
+        private System.Windows.Forms.Label LastBillingAdressLabel;
+        private System.Windows.Forms.Button OpenBillingAdress;
 
         #endregion
     }
--- EasyBillingService/MainWindow.cs
+++ EasyBillingService/MainWindow.cs
@@ -1,27 +1,140 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Reflection;
 using System.Windows.Forms;
 using EasyBillingService.Extensions;
 using Microsoft.Office.Interop.Excel;
-using MiniExcelLibs;
-using MiniExcelLibs.Attributes;
-using MiniExcelLibs.OpenXml;
-using Application = Microsoft.Office.Interop.Excel.Application;
-using Excel = Microsoft.Office.Interop.Excel;
 
 namespace EasyBillingService
 {
     public partial class MainWindow : Form
     {
+        private string _path;
+
+        private string _lastOpenedFile;
+
+        private const string CONFIGURATIONFILEPATH = "..\\..\\configuration.cfg";
+
+        private string _lastOpenedFileText = "lastOpenedFile = ";
+
+        private double currentID;
+        
         public MainWindow()
         {
             InitializeComponent();
+            InitializeFormerFilePaths();
+            RetrieveLastBillingNumber();
+        }
+
+        private void InitializeFormerFilePaths()
+        {
+            var configurationPath = CONFIGURATIONFILEPATH;
+            if (File.Exists(configurationPath))
+            {
+                
+                using (var stream = File.OpenRead(configurationPath))
+                {
+                    var lines = File.ReadLines(configurationPath);
+
+                    foreach (var line in lines)
+                    {
+                        if (line.StartsWith(_lastOpenedFileText))
+                        {
+                            _lastOpenedFile = line.Substring(_lastOpenedFileText.ToCharArray().Length);
+                            break;
+                        }
+                    }
+                }
+            }
+            
+            textBox1.Text = _lastOpenedFile;
+        }
+
+        private void SetNewFilePath(string path)
+        {
+            var newValue = _lastOpenedFileText+ path;
+            var oldValue = _lastOpenedFileText + (_lastOpenedFile);
+            if (File.Exists(CONFIGURATIONFILEPATH))
+            {
+                var text = "";
+                using (var stream = File.OpenRead(CONFIGURATIONFILEPATH))
+                {
+                    text = File.ReadAllText(CONFIGURATIONFILEPATH);
+                }
+                
+                text = text.Replace(oldValue,newValue);
+                File.WriteAllText(CONFIGURATIONFILEPATH,text);
+            }
+
+            _lastOpenedFile = path;
         }
         
-        
+        void RetrieveLastBillingNumber()
+        {
+           
+            var excelApplication = new Microsoft.Office.Interop.Excel.Application();
+            
+            
+            object missing = Missing.Value;
+            var path = _lastOpenedFile;
+            
+            Workbook workbook = excelApplication.Workbooks.Open(path);
+
+            DateTimeFormatInfo fmt = (new CultureInfo("hr-HR")).DateTimeFormat;
+
+            var sheet = workbook.ActiveSheet as Worksheet;
+            var billingIds = new List<(double idNumber, String date, String reciepient)>(); //ids are set here as numbers 
+            
+            var grid = sheet.Range["A1", "C1000"].Value as object[,];
+
+            
+            for (int  i = 1;  i < grid.GetLength(0); i++)
+            {
+                
+                double value = 0;
+                if(!double.TryParse((String)grid[i, 1], out value))
+                {
+                    continue;
+                }
+                
+                var date = (DateTime)grid[i, 2];
+                
+                var entry = (value,date.ToString("d", fmt),(String)grid[i,3]);
+                
+                billingIds.Add(entry);
+            }
+
+            double maximum = 0;
+
+            if (!billingIds.Any())
+            {
+                return;
+            }
+            var newestEntry = billingIds.OrderByDescending(x => x.idNumber).First();
+
+            LastBillingNumber.Text = newestEntry.idNumber.ToString();
+
+            RecipientText.Text = newestEntry.reciepient;
+
+            DateText.Text = newestEntry.date;
+            
+
+            currentID = newestEntry.idNumber + 1;
+            
+            
+            workbook.Save();
+            workbook.Close(SaveChanges: false);
+            workbook = null;
+            excelApplication.Quit();
+            excelApplication = null;
+            
+            
+        }
         static void Test()
         {
            
@@ -42,8 +155,6 @@ namespace EasyBillingService
             sheet.SetCellValue<double>("B2", cell);
             
             workbook.Save();
-            
-            
             workbook.Close(SaveChanges: false);
             workbook = null;
             excelApplication.Quit();
@@ -51,38 +162,22 @@ namespace EasyBillingService
             
             
 		}
-
+        
         private void button1_Click(object sender, EventArgs e)
         {
+            var dialog = new System.Windows.Forms.OpenFileDialog();
+
+            dialog.ShowDialog();
             
-            Test();
-            /*label1.Text = " clicked the button";
-
-            var table = new List<Entry>();
+            textBox1.Text = dialog.FileName;
             
-            var config = new OpenXmlConfiguration()
-            {
-                TableStyles = MiniExcelLibs.OpenXml.TableStyles.None,
-            };
-            
-            using (var stream = File.OpenRead("test.xlsx"))
-            { 
-                table = MiniExcel.Query<Entry>("test.xlsx").ToList();
-                
-           
 
+           SetNewFilePath(dialog.FileName);
+        }
 
-                table = table.Where(entry => entry.Name != null).ToList();
-
-                table[1].Value = 1;
-            }
-
-            MiniExcel.SaveAs("test.xlsx", table,true,"Sheet1",ExcelType.XLSX,null,true);*/
-        
-
-           // File.Replace("test_temp.xlsx", "test.xlsx", "test.xlsx", false);
-            
-           // File.Delete("test_temp.xlsx");
+        private void SetValue_Click(object sender, EventArgs e)
+        {
+            RetrieveLastBillingNumber();
         }
 
 
@@ -96,9 +191,16 @@ namespace EasyBillingService
         {
             
         }
-        
-        
-        
-       
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void BillingNumber_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
